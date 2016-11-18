e7ffb51815d207fd0f8bf6625f61b7e543b32a8f
Frank Bi <frankbi@Franks-MacBook-Air.local>
2016-11-18 11:58:57 -0800

    production readme

--- bundle.js
+++ bundle.js
@@ -348,18 +348,6 @@
 	  _createClass(Obstacle, [{
 	    key: "populateObstacles",
 	    value: function populateObstacles() {
-	      // let obstacle = new createjs.Shape();
-	      // let randomY = Math.random() * 4 * 100;
-	      // let randomHeight = Math.random() * 4 * 100;
-	      // obstacle.graphics.beginFill("black").drawRect(800,randomY,50,randomHeight);
-	      // obstacle.x = 800;
-	      // obstacle.y = randomY;
-	      // obstacle.width = 50;
-	      // obstacle.height = randomHeight;
-	      // // debugger
-	      // this.stage.addChild(obstacle);
-	
-	
 	      this.obstacles = new createjs.Container();
 	      this.obstacles.name = "obstacles";
 	      this.obstacles.x = 0;
@@ -367,21 +355,8 @@
 	      this.createObstacle(500);
 	      this.createObstacle(800);
 	      this.createObstacle(1100);
-	      // let obstacle = new createjs.Shape();
-	      // obstacle.x = 0;
-	      // obstacle.y = 0;
-	      // obstacle.width = 100;
-	      // obstacle.height = Math.random() * 4 * 100;
-	      // obstacle.graphics.beginFill("black").drawRect(obstacle.x,obstacle.y,obstacle.width,obstacle.height);
-	      // debugger
-	      // this.obstacles.addChild(obstacle);
+	
 	      this.stage.addChild(this.obstacles);
-	    }
-	  }, {
-	    key: "reset",
-	    value: function reset() {
-	      this.stage.removeChild(this.obstacles);
-	      this.populateObstacles();
 	    }
 	  }, {
 	    key: "move",
@@ -393,6 +368,12 @@
 	      if (this.obstacles.x + 2500 < 0) {
 	        this.reset();
 	      }
+	    }
+	  }, {
+	    key: "reset",
+	    value: function reset() {
+	      this.stage.removeChild(this.obstacles);
+	      this.populateObstacles();
 	    }
 	  }, {
 	    key: "createObstacle",
